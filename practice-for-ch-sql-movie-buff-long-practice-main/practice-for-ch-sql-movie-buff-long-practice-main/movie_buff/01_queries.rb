5b2cf8078ebe0508f848c8e9c4cb4d42b476e914
Julian Cardone <juliancardone1@gmail.com>
2022-10-14 13:49:59 -0400

    second commit

--- practice-for-ch-sql-movie-buff-long-practice-main/practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -7,6 +7,8 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
 
+  Movie.select(:id, :title, :score).where("score BETWEEN 2 AND 3")
+
 end
 
 def harrison_ford
@@ -20,6 +22,8 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+
+  
 
 end
 
